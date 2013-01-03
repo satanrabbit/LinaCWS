f3d41a4e1ff2f955506fec8941eaea699f8e495c
SatanRabbit <satanrabbit@gmail.com>
2013-01-03 10:39:09 +0800

    数据库、链接类
    1、建立完成数据库
    2、配置连接字符串
    3、建立连接类

---
+++ LinaCWS/LinaCWS/COM/sql.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace LinaCWS.COM
+{
+    /// <summary>
+    /// 数据查询类
+    /// </summary>
+    public class sql
+    {
+        public sql()
+        {
+            connStr = ConfigurationManager.ConnectionStrings["CWSConnStr"].ConnectionString;
+            conn = new OleDbConnection(connStr);
+            cmd = conn.CreateCommand();
+            da = new OleDbDataAdapter(cmd);
+            ds = new DataSet();
+        }
+        public string connStr { get; set; }
+        public OleDbConnection conn { get; set; }
+        public OleDbCommand cmd { get; set; }
+        public OleDbDataAdapter da { get; set; }
+        public OleDbDataReader dr { get; set; }
+        public DataSet ds { get; set; }
+
+
+    }
+}
